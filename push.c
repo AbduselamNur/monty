ca666ce1c3615e0b0ee7a27789f00e032046615c
abduselamNur <abduselamnurhussen626@gmail.com0>
2023-05-24 04:13:22 -0700

    first task

---
+++ push.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
+}
